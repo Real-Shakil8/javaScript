44dcc4c280a99486d19ab6665808cdb787da8f6b
ShakiL <realshakil8@gmail.com>
2022-11-30 12:22:33 +0600

    array name change now array name is arrays

--- array.js
+++
@@ -1,7 +0,0 @@
-const cars = ['BMW', 'HONDA', 'VOLVO']; //arry declear
-//document.getElementById('array').innerHTML = cars;
-console.log(cars[0]);
-cars[0] = 'HONDA';
-console.log(cars[0]);
-
-
---
+++ arrays.js
@@ -0,0 +1,7 @@
+const fruits = ['Apple', 'Banana', 'Painapel', 'Orange', 'Mango']; //arry declear
+//document.getElementById('array').innerHTML = cars;
+console.log(fruits[0]);
+console.log(`fruits item are `+ fruits.length);
+console.log(`fruits last item is `+ fruits(fruits.length -1));
+
+
