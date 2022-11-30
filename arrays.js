e47686a0e5fe0617642d6b393a438d734970f09b
ShakiL <realshakil8@gmail.com>
2022-11-30 13:05:25 +0600

    shakil

--- arrays.js
+++ arrays.js
@@ -1,7 +1,23 @@
 const fruits = ['Apple', 'Banana', 'Painapel', 'Orange', 'Mango']; //arry declear
+// fruits[10] = 'jackfruit';
 //document.getElementById('array').innerHTML = cars;
-console.log(fruits[0]);
+// console.log(fruits[0]);
+let fLen = fruits.length;
+//push array
+fruits.push('Lemon');
 console.log(`fruits item are `+ fruits.length);
-console.log(`fruits last item is `+ fruits(fruits.length -1));
+console.log(`fruits last item is `+ fruits[fruits.length -1]);
 
+// for loop with array
+// for (i=0; i<fLen; i++){
+//     console.log(fruits[i]);
+// };
+// console.log('for loop end');
+// console.log('');
 
+// //Array.forEach function
+// function myfunction(item){
+//     console.log(item);
+// };
+// fruits.forEach(myfunction);
+// console.log('Finished');
