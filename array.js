acdfbc8a225545d5158466cecca9207c3367d49a
ShakiL <realshakil8@gmail.com>
2022-11-30 07:33:39 +0600

    arry decleare nuber change

--- array.js
+++ array.js
@@ -1,3 +1,7 @@
 const cars = ['BMW', 'HONDA', 'VOLVO']; //arry declear
-document.getElementById('array').innerHTML = cars;
+//document.getElementById('array').innerHTML = cars;
+console.log(cars[0]);
+cars[0] = 'HONDA';
+console.log(cars[0]);
+
 
