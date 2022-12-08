2d3ba0ce73b8783817df54176dd85c6391b40faf
ShakiL <realshakil8@gmail.com>
2022-12-08 10:14:16 +0600

    commit all

--- array litaral.js
+++ array litaral.js
@@ -1,2 +1,8 @@
-let a = 5;
-console.log(a);
+const numbers = [2,4,45,4,9,16,25];
+const fruits = ['Banana','Painapel','Mango','Orange'];
+
+function myFunction(value){
+    return value > 5;
+};
+const result = numbers.findIndex(myFunction);
+console.log(result);
