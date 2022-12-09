677d45f5f58fcb87486c94ff8212585dc7616f57
ShakiL <realshakil8@gmail.com>
2022-12-09 15:13:39 +0600

    friday

--- date.js
+++ date.js
@@ -1,1 +1,10 @@
-console.log('Shakil Khan');
+const d = new Date();
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+// console.log(d.getMonth() + 1);
+console.log(d);
+console.log(days[d.getDay()]);
+console.log(months[d.getMonth()]);
+
+console.log(d.setMonth(15));
